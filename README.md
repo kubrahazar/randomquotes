2d88becca0e13e33381dfe83f21d71453c772fea
kubrahazar <kubrahazar6@gmail.com>
2022-02-10 14:59:10 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # randomquotes
 Nodejs application that prints random quotes.
+
+## Favorite quotation
+
+"The way to get started is to quit talking and begin doing."
+
+-Walt Disney
---
+++ index.js
@@ -0,0 +1,6 @@
+let quotes = [
+    `"The way to get started is to quit talking and begin doing." - Walt Disney`,
+    `"The future belongs to those who believe in the beauty of their dreams." - Eleanor Roosevelt`,
+    `"Whoever is happy will make others happy too.""Whoever is happy will make others happy too." - Anne Frank`]
+let index = Math.floor((quotes.length * Math.random()))
+console.log(quotes[index])
